09d0c347980a25957ec9eaefb09212302f9c8e7b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-05 19:26:55 -0700

    make program internal and discard return values

--- SharpClipboard.Tests.WinForms/Program.cs
+++ SharpClipboard.Tests.WinForms/Program.cs
@@ -7,7 +7,7 @@ namespace SharpClipboard.Tests.WinForms
     /// <summary>
     /// Default program class.
     /// </summary>
-    public static class Program
+    internal static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -16,10 +16,20 @@ namespace SharpClipboard.Tests.WinForms
         [STAThread]
         public static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            _ = Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            MainForm form = null;
+            try
+            {
+                form = new MainForm();
+                Application.Run(form);
+            }
+            finally
+            {
+                form?.Dispose();
+            }
         }
     }
 }
